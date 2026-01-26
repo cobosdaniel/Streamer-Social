a012c969b5a058e5dda0ac383d8e28cf70c88aca
Daniel Cobos <drcobos@asu.edu>
2026-01-26 15:22:54 -0700

    DC1 - Script allows for a list of all the twitch's user custom made channel point redeems to be tracked, reward_id is manually set. Only one redeem can be tracked at the moment

---
+++ track_redemption.py
@@ -0,0 +1,80 @@
+import json
+import websocket
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv("twitch_oauth_token.env")
+
+CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
+OAUTH_TOKEN = os.getenv("TWITCH_OAUTH_TOKEN")
+BROADCASTER_ID = os.getenv("TWITCH_BROADCASTER_ID")
+# REWARD_ID = os.getenv("REWARD_ID")
+
+HEADERS = {
+    "Client-ID": CLIENT_ID,
+    "Authorization": f"Bearer {OAUTH_TOKEN}",
+    "Content-Type": "application/json"
+}
+
+url = f"https://api.twitch.tv/helix/channel_points/custom_rewards?broadcaster_id={BROADCASTER_ID}"
+resp = requests.get(url, headers=HEADERS)
+resp.raise_for_status()
+
+data = resp.json()["data"]
+
+
+rewards_by_id = { reward["id"]: reward for reward in data }
+print(f"Loaded {len(rewards_by_id)} rewards.")
+
+# To subscribe and track ONE particular channel point redemption.
+def subscribe(session_id):
+    url = "https://api.twitch.tv/helix/eventsub/subscriptions"
+    payload = {
+        "type": "channel.channel_points_custom_reward_redemption.add",
+        "version": "1",
+        "condition": {
+            "broadcaster_user_id": BROADCASTER_ID,
+            "reward_id": "794663b9-bb47-4af9-8ea6-a87c7a86b2d0"
+        },
+        "transport": {
+            "method": "websocket",
+            "session_id": session_id
+        }
+    }
+    requests.post(url, headers=HEADERS, json=payload)
+
+def on_message(ws, message):
+    data = json.loads(message)
+
+    if data["metadata"]["message_type"] == "session_welcome":
+        session_id = data["payload"]["session"]["id"]
+        subscribe(session_id)
+
+    elif data["metadata"]["message_type"] == "notification":
+        event = data["payload"]["event"]
+        username = event["user_name"]
+        
+        reward_id = event["reward"]["id"]
+        reward_title = rewards_by_id.get(reward_id, {}).get("title", "Unknown Reward")
+        print(f"{reward_title} Redeemed by: {username}")
+
+def on_error(ws, error):
+    print("Error:", error)
+
+def on_close(ws, close_status_code, close_msg):
+    print(f"Connection closed: {close_status_code} - {close_msg}")
+
+
+def on_open(ws):
+    print("Connected to Twitch EventSub")
+
+if __name__ == "__main__":
+    ws = websocket.WebSocketApp(
+        "wss://eventsub.wss.twitch.tv/ws",
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close,
+        on_open=on_open
+    )
+    ws.run_forever()
