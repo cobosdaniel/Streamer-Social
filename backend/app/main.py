d8659cd1ddadeae84f829689cacde7a610146751
Daniel Cobos <drcobos@asu.edu>
2026-04-03 14:08:05 -0700

    added requirements.txt to backend and changed id.py name into main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -58,7 +58,7 @@ manager = ConnectionManager()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
